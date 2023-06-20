eb0fa9713707c413b189bf7f1177b19d637d13fc
Artem-Grushchinskiy <122301492+Artem-Grushchinskiy@users.noreply.github.com>
2023-06-20 13:27:37 +0200

    upd contacts after mentor check

--- contacts.js
+++ contacts.js
@@ -35,7 +35,7 @@ async function getContact(id) {
   try {
     const contacts = await readContacts();
     const contact = contacts.find((c) => c.id === id);
-    return contact;
+    return contact || null;
   } catch (error) {
     throw error;
   }
@@ -56,9 +56,13 @@ async function addContact(name, email, phone) {
 async function removeContact(id) {
   try {
     const contacts = await readContacts();
-    const updatedContacts = contacts.filter((contact) => contact.id !== id);
-    await writeContacts(updatedContacts);
-    return updatedContacts;
+    const contactIndex = contacts.findIndex((c) => c.id === id);
+    if (contactIndex === -1) {
+      return null;
+    }
+    const removedContact = contacts.splice(contactIndex, 1)[0];
+    await writeContacts(contacts);
+    return removedContact;
   } catch (error) {
     throw error;
   }
